13a15ba1fff3c8ff691c4ef9a80d910912ff3005
shakourbin.hsh@gmail.com <shakourbin.hsh@gmail.com>
2019-12-04 15:55:19 +0330

    ss

---
+++ BeautySalon/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautySalon.Models
+{
+    public class Customer :BaseEntity
+    {
+        public Customer() : base()
+        {
+        }
+
+        public string Name { get; set; }
+        public string FamilyName { get; set; }
+    }
+}
---
+++ BeautySalon/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautySalon.Models
+{
+    public class Employee : BaseEntity
+    {
+        public Employee() :base()
+        {
+        }
+    }
+}
